2e312bf0069e60ef387c0712ed9d0fffee8bd014
Ahmet <muratahmetkorkmaz@hotmail.com>
2020-09-19 18:58:52 +0300

    add soft delete to all models

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Card extends Model
 {
+    use SoftDeletes;
+
     public function column()
     {
         return $this->belongsTo(Column::class);
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Team extends Model
 {
+    use SoftDeletes;
+
     public function users()
     {
         return $this->belongsToMany(User::class, 'team_user');
