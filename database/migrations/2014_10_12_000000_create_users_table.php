245ddcebb262d8852d628f8b389a1d0ccbaeb9fb
Ahmet <muratahmetkorkmaz@hotmail.com>
2020-09-19 17:34:36 +0300

    Update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('photo_id')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
