6b13e762213067c9e952255358bc290bed6ca5fe
Ahmet <ahmetkorkmaz3453@gmail.com>
2021-01-03 22:17:19 +0300

    l5 swagger implement

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info (
+ *     title="Trello Clone API",
+ *     version="1.0.0"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ResponseTrait;
